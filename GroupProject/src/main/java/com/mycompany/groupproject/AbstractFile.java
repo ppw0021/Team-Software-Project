cb75e4b621e0b6da34cbcee6aa1f8d747e84db84
TheWingu <cmx0750@autuni.ac.nz>
2023-09-02 14:11:47 +1200

    comments

--- GroupProject/src/main/java/com/mycompany/groupproject/AbstractFile.java
+++ GroupProject/src/main/java/com/mycompany/groupproject/AbstractFile.java
@@ -4,12 +4,12 @@ abstract class AbstractFile {
 
     private String DocumentName;
     private String DocumentEditsName;
-    //private String Password;
+    
 
     public AbstractFile(String documentName, String documentEditsName) {
         this.DocumentName = documentName;
         this.DocumentEditsName = documentEditsName;
-        //this.Password = password;
+        
     }
 
     public void create(String DocumentName) {
@@ -25,13 +25,5 @@ abstract class AbstractFile {
         this.DocumentName = DocumentName;
         this.DocumentEditsName = ("EDITS_" + DocumentName);
     }
-/*
-    public String getPassword() {
-        return Password;
-    }
 
-    public void setPassword(String Password) {
-        this.Password = Password;
-    }
-*/
 }
