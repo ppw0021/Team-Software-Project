caaa65a7edc9c73c9ba50a8d4837fff1f60a00f0
TheWingu <cmx0750@autuni.ac.nz>
2023-09-03 20:15:34 +1200

    test things lol

--- GroupProject/src/main/java/com/mycompany/groupproject/CreateFile.java
+++ GroupProject/src/main/java/com/mycompany/groupproject/CreateFile.java
@@ -12,23 +12,23 @@ public class CreateFile extends AbstractFile {
     }
 
     @Override
-    public void create(String DocumentName) {
+    public void create(String DocumentName) {//Used to create files
         try {
-            File primaryFile = new File(DocumentName + ".txt");
-            File editsFile = new File("EDITS_" + DocumentName + ".txt");
+            File primaryFile = new File(DocumentName + ".txt");//Creates a .txt file
+            File editsFile = new File("EDITS_" + DocumentName + ".txt");//Creates a edit.txt file
             if (primaryFile.createNewFile() && editsFile.createNewFile()) {
                 System.out.println("File created: " + primaryFile.getName());
                 FileWriter primaryWriter = new FileWriter(primaryFile, true);
                 System.out.println("Please add your information");
-                Scanner tx=new Scanner(System.in);
-                String text=tx.nextLine();
+                Scanner tx = new Scanner(System.in);
+                String text = tx.nextLine();//Allows the user to add text 
                 primaryWriter.write(text);
                 primaryWriter.close();
                 System.out.println("Text added to file: " + primaryFile.getName());
             } else {
                 System.out.println("File already exists.");
             }
-        } catch (IOException e) {
+        } catch (IOException e) {//Catches errors
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
